6b5e707037116f04bb33caee690165f5df650f02
Mateus Moraes <cmateusmoraes@gmail.com>
2017-08-06 21:25:19 -0300

    Comit com a finalização da implementação das funcionalidades

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (    
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa Hístória</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+
+        <h2>Missão e Valores</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
